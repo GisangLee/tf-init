27fc7dfe69fa24dfa59f6cf61ee261a1fa64fe61
GisangLee <masterkorea01@naver.com>
2023-05-04 12:36:57 +0900

    terraform block 수정

--- main.tf
+++ main.tf
@@ -14,16 +14,13 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "dude_tf_test"
 
-    # workspaces {
-    #   name = local.workspace_name
-    # }
-  }
-}
-
-# workspace name 설정
-default = {
-  workspaces = {
-    name = local.workspace_name
+    workspaces {
+      prefix = var.project_name
+      tags = {
+        environment = var.env
+        project     = var.project_name
+      }
+    }
   }
 }
 
