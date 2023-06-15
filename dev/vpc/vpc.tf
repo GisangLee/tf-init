70abc67984032dee778cbe9de5a8fbde4f0b4b93
GisangLee <masterkorea01@naver.com>
2023-06-16 07:55:46 +0900

    vpc

--- dev/vpc/vpc.tf
+++ dev/vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-    source = "../../modules/network"
-    ENV = var.ENV
-    AWS_REGION = var.AWS_REGION
-    PROJECT_NAME = var.PROJECT_NAME
+  source       = "../../modules/network"
+  ENV          = var.ENV
+  AWS_REGION   = var.AWS_REGION
+  PROJECT_NAME = var.PROJECT_NAME
 }
