1a351c7b8f325f4549362e20779a2f55cf6111b4
GisangLee <masterkorea01@naver.com>
2023-06-16 08:19:06 +0900

    provider2

--- dev/vpc/main.tf
+++ dev/vpc/main.tf
@@ -13,3 +13,9 @@ terraform {
   #   # dynamodb_table = "terraform-lock"
   # }
 }
+# Configure the AWS Provider
+provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACESST_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
