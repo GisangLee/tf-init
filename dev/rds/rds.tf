143c50beda9b0e2423d3e4b0281ecf3856554208
GisangLee <masterkorea01@naver.com>
2023-06-17 21:02:03 +0900

    data source

--- dev/rds/rds.tf
+++ dev/rds/rds.tf
@@ -1,8 +1,27 @@
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "../../module/network/outputs.tf"
+  }
+}
+
+data "terraform_remote_state" "sm" {
+  backend = "local"
+
+  config = {
+    path = "../../module/secret_manager/outputs.tf"
+  }
+}
+
 module "rds" {
   source = "../../modules/rds"
   ENV = var.ENV
-  AZ_LIST = module.vpc.az_list
-  DB_PWD = module.secret_manager.db-pwd
-  SUBNET_IDS = module.vpc.subnet_list
+  # AZ_LIST = module.vpc.az_list
+  AZ_LIST = data.terraform_remote_state.vpc.outputs.az_list
+  DB_PWD = data.terraform_remote_state.sm.outputs.db-pwd
+  SUBNET_IDS = data.terraform_remote_state.vpc.outputs.subnet_list
+  # DB_PWD = module.secret_manager.db-pwd
+  # SUBNET_IDS = module.vpc.subnet_list
   PROJECT_NAME = var.PROJECT_NAME
 }
--- dev/sg/sg.tf
+++ dev/sg/sg.tf
@@ -1,7 +1,16 @@
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "../../module/network/outputs.tf"
+  }
+}
+
 module "sg" {
     source = "../../modules/sg"
     ENV = var.ENV
     AWS_REGION = var.AWS_REGION
-    VPC_ID = module.vpc.vpc_id
+    # VPC_ID = module.vpc.vpc_id
+    VPC_ID = data.terraform_remote_state.vpc.vpc.outputs.vpc_id
     PROJECT_NAME = var.PROJECT_NAME
 }
