3f263d6faa1e1d0bc7387c0d1778d6c9f1459a3f
GisangLee <masterkorea01@naver.com>
2023-06-17 21:07:27 +0900

    data source

--- dev/sg/sg.tf
+++ dev/sg/sg.tf
@@ -1,5 +1,5 @@
 data "terraform_remote_state" "vpc" {
-  backend = "local"
+  backend = "s3"
 
   config = {
     path = "../../module/network/outputs.tf"
@@ -11,6 +11,6 @@ module "sg" {
     ENV = var.ENV
     AWS_REGION = var.AWS_REGION
     # VPC_ID = module.vpc.vpc_id
-    VPC_ID = data.terraform_remote_state.vpc.vpc.outputs.vpc_id
+    VPC_ID = data.terraform_remote_state.vpc.outputs.vpc_id
     PROJECT_NAME = var.PROJECT_NAME
 }
